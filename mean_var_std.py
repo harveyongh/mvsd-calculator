0220d4d854a0694c602e386b050bcd9b6171fc18
harvey desktop <harveygit@pm.me>
2024-06-08 22:33:49 -0500

    Progress main file

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
+    arr = np.array(list).reshape(3,3)
+    calculations = {'mean':[arr.mean(axis=0), arr.mean(axis=1), arr.mean()],
+                    'variance':[arr.var(axis=0), arr.var(axis=1), arr.var()],
+                    'standard deviation':[arr.std(axis=0), arr.std(axis=1), arr.std()],
+                    'max':[arr.max(axis=0), arr.max(axis=1), arr.max()],
+                    'min':[arr.min(axis=0), arr.min(axis=1), arr.min()],
+                    'sum':[arr.sum(axis=0), arr.sum(axis=1), arr.sum()]}
     return calculations
+
+
+data = calculate([0,1,2,3,4,5,6,7,8])
+print(data)
